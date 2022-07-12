034fd4788ac655da1ad8e4b675b8ab60096538af
kentezrabetita <kentezrabetita@gmail.com>
2022-07-13 02:10:18 +0800

    add nprogress route loading animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../styles/nprogress.css';
+import nProgress from 'nprogress';
+import Router from 'next/router';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
