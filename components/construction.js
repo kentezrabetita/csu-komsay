948719d6f1e96197962320a6bb71ef3d6c4e6dcb
kentezrabetita <kentezrabetita@gmail.com>
2022-07-20 03:40:45 +0800

    update construction page layout

--- components/construction.js
+++ components/construction.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
+import MainLayout from './layout';
 
 export default function Construction() {
   return (
-    <div className='container flex flex-col p-6 mx-auto text-center'>
-      <motion.div
-        className='p-4'
-        animate={{
-          y: 10,
-        }}
-        transition={{ repeatType: 'mirror', repeat: Infinity, duration: 1 }}
-      >
-        <Image
-          src='/under_construction.svg'
-          width={600}
-          height={600}
-          alt='csu icon'
-        />
-      </motion.div>
-      <div className='text-lg md:text-2xl'>
-        The site is still under construction. Thank you for your patience.
+    <MainLayout>
+      <div className='container h-[100vh] flex flex-col p-6 mx-auto text-center'>
+        <motion.div
+          className='p-4'
+          animate={{
+            y: 10,
+          }}
+          transition={{ repeatType: 'mirror', repeat: Infinity, duration: 1 }}
+        >
+          <Image
+            src='/under_construction.svg'
+            width={600}
+            height={600}
+            alt='csu icon'
+          />
+        </motion.div>
+        <div className='text-lg md:text-2xl'>
+          The site is still under construction. Thank you for your patience.
+        </div>
       </div>
-    </div>
+    </MainLayout>
   );
 }
