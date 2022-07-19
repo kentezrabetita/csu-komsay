ea8c19a59fc7a9f1e77fb7ef3c81f5ea1c6bf1ff
kentezrabetita <kentezrabetita@gmail.com>
2022-07-20 03:40:25 +0800

    add hero image

--- components/landing.js
+++ components/landing.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import Hero from './hero';
 import MainLayout from './layout';
+import { motion } from 'framer-motion';
 
 export default function Landing() {
   return (
     <MainLayout>
-      <section className='flex flex-col h-screen'>
-        <Hero />
+      <section className='h-[80vh]'>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8 }}
+          className='flex justify-center h-full bg-center bg-no-repeat bg-cover bg-saiyans'
+        >
+          <div className='inset-0 flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50'>
+            <h1 className='text-2xl font-bold text-center text-white uppercase md:text-5xl'>
+              Caraga State University - Computer Science
+            </h1>
+          </div>
+        </motion.div>
       </section>
     </MainLayout>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        saiyans: "url('../public/images/saiyans.jpg')",
+      },
+    },
   },
   plugins: [],
 };
